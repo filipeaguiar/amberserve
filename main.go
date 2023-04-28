35f82d96691d2a3f6b525275900b44dd47d22dda
Filipe Aguiar <filipe_aac@yahoo.com.br>
2023-04-28 17:49:47 -0300

    alterações e compilação final

--- main.go
+++ main.go
@@ -51,7 +51,7 @@ func main() {
 		panic(err)
 	}
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "static/index2.html")
+		http.ServeFile(w, r, "static/index.html")
 	})
 
 	type Platform struct {
